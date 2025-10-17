bf9f224e6bdb4032e99c65ec52051d86d2eb0803
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2025-10-17 09:24:32 +0300

    phrase set refactored for easy of redefining

--- flakyzavr/__init__.py
+++ flakyzavr/__init__.py
@@ -3,10 +3,10 @@ from ._flakyzavr_plugin import Flakyzavr
 from ._flakyzavr_plugin import FlakyzavrPlugin
 from ._messages import EN_REPORTING_LANG
 from ._messages import RU_REPORTING_LANG
-from ._messages import ReportingLangSet
+from ._messages import BaseReportingPhrases
 
 __version__ = get_version()
 __all__ = (
     "Flakyzavr", "FlakyzavrPlugin",
-    "ReportingLangSet", "RU_REPORTING_LANG", "EN_REPORTING_LANG"
+    "BaseReportingPhrases", "RU_REPORTING_LANG", "EN_REPORTING_LANG"
 )
