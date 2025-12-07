63e5872e5b07bbc3e0d45187936ac76e299a243d
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2025-12-08 01:03:49 +0300

    cleanup

--- tests/helpers/temp_file.py
+++ tests/helpers/temp_file.py
@@ -1,22 +1,12 @@
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Union
 
 
 @contextmanager
-def temp_file(file_path: Union[str, Path], content: str):
+def temp_file(file_path: str | Path, content: str):
     """
     Context manager that creates a temporary file with specified content
     and removes it on exit.
-
-    :param file_path: Path where the file should be created
-    :param content: Content to write to the file
-
-    Usage:
-        with temp_file("rel_path/to/file.txt", "Hello World") as rel_path:
-            # file exists here
-            pass
-        # file is deleted here
     """
     path = Path(file_path)
 
